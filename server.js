9330289cb997fb7d3755ea56b1c254520e60975f
mammumammi <ashinmammu@gmail.com>
2025-10-10 22:41:22 +0530

    changed stuffs

--- server.js
+++ server.js
@@ -200,13 +200,14 @@ app.post("/priceSummarize",async (req,res) => {
     }
 });
 
+// Serve static files from the dist directory
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+// Catch-all handler: send back React's index.html file for any non-API routes
+app.get('/:path*', (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
-
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
